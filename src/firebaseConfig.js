9619a7fb3b4586cb21a406368f1a35d644ec8c07
Asami Chiruko <asami.chiruko@gmail.com>
2025-07-22 16:42:12 +0900

    dev 環境でのみ emulators を使うように修正し本番用に .env.production ファイルを追加

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+import { connectAuthEmulator, getAuth } from "firebase/auth"
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore"
 
-// .envから読み込む
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -13,5 +12,16 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+if (import.meta.env.VITE_USE_FIREBASE_EMULATOR === "true") {
+  console.log("CONNECT AS DEV")
+  const authUrl = import.meta.env.VITE_USE_AUTH_EMULATOR_URL
+  connectAuthEmulator(auth, authUrl)
+  const firestoreHost = import.meta.env.VITE_USE_FIRESTORE_EMULATOR_HOST
+  const firestorePort = parseInt(import.meta.env.VITE_USE_FIRESTORE_EMULATOR_PORT)
+  connectFirestoreEmulator(db, firestoreHost, firestorePort)
+}
+
+export { auth, db }
